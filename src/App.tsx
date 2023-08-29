9e72668a1a40e61ea5e21fe4ad03f792cf47db31
dongtianllll <liuxiaodong@nstc.com.cn>
2023-08-29 21:42:51 +0800

    feature: add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+import {ColorPicker, ConfigProvider, Space, theme} from "antd";
+import {StyleProvider, px2remTransformer} from '@ant-design/cssinjs';
+import LoginForm from "./pages/LoginForm.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
+const px2rem = px2remTransformer({
+    rootValue: 16, // 16px = 1rem; @default 16
+});
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+const App = () => {
+    const [primaryColor, setPrimaryColor] = useState('#0052cc');
+
+    return <ConfigProvider
+        theme={{
+            algorithm: theme.compactAlgorithm,
+            token: {
+                colorPrimary: primaryColor,
+            }
+        }}
+    >
+        <ColorPicker value={primaryColor} onChangeComplete={(color) => setPrimaryColor(color.toHexString())}/>
+        <StyleProvider transformers={[px2rem]}>
+            <Space>
+                <LoginForm/>
+            </Space>
+        </StyleProvider>
+    </ConfigProvider>
 }
 
 export default App
